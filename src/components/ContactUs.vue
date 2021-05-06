54b95d6054a61013cf97c2ae6da53eb913d05f08
Camille Rajon <camillerajon@gmail.com>
2021-05-06 13:20:55 +0200

    Formulaire test 2

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -11,7 +11,8 @@
             </div>
             <div class="row">
                 <div class="col-lg-8 offset-lg-2">
-                    <form class="contact-form" name="contact" method="POST" data-netlify="true">
+                    <form class="contact-form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+                    <input type="hidden" name="form-name" value="contact" />
                         <div class="row">
                             <div class="col-12">
                                 <input name="name" type="text" placeholder="Your Name">
