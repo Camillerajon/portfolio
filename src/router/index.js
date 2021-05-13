c35ca997e529353e186f0062046ddb0a5aafb9d1
Camille Rajon <camillerajon@gmail.com>
2021-05-13 12:37:55 +0200

    indexation

--- src/router/index.js
+++ src/router/index.js
@@ -42,3 +42,11 @@ const router = new VueRouter({
 })
 
 export default router
+
+const RobotstxtPlugin = require("robotstxt-webpack-plugin");
+ 
+const options = {}; // see options below
+ 
+module.exports = {
+  plugins: [new RobotstxtPlugin(options)]
+};
